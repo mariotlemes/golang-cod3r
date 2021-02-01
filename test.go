760c23d247a174d9811b5e356e2b823d61344bc9
Mario Teixeira Lemes <mariolpu@gmail.com>
2021-02-01 09:52:08 -0300

    Update code

--- test.go
+++ test.go
@@ -1,7 +1,14 @@
+// Programas executáveis iniciam pelo pacote main
 package main
 
 import "fmt"
 
+/**
+Esta função é executada
+ */
+
 func main() {
-	fmt.Print("Teste de print! \n")
+
+	fmt.Print("\nTeste de print! \n")
+	fmt.Print("Primeiro programa! \n")
 }
